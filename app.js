498ba5eb6182cd46455f492a1bf5eb1db4e471ae
Evan Ochs <evanochs@Evans-MacBook-Pro-2.local>
2020-12-10 11:23:06 -0500

    working key strokes and tetromino functions

---
+++ app.js
@@ -0,0 +1,133 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  let squares = Array.from(grid.querySelectorAll("div"));
+  const width = 10;
+  const height = 20;
+  let currentPosition = 4;
+
+  function control(e) {
+    if (e.keyCode === 39) {
+      moveRight();
+    } else if (e.keyCode === 38) {
+      rotate();
+    } else if (e.keyCode === 37) {
+      moveLeft();
+    } else if (e.keyCode === 40) {
+      moveDown();
+    }
+  }
+
+  document.addEventListener("keyup", control);
+
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
+
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+  ];
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
+  ];
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+  ];
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+  ];
+
+  const theTetrominoes = [
+    lTetromino,
+    zTetromino,
+    tTetromino,
+    oTetromino,
+    iTetromino,
+  ];
+
+  //Randomly select Tetromino
+  let random = Math.floor(Math.random() * theTetrominoes.length);
+  let currentRotation = 0;
+  let current = theTetrominoes[random][currentRotation];
+
+  //draw the shape
+  function draw() {
+    current.forEach((index) =>
+      squares[currentPosition + index].classList.add("block")
+    );
+  }
+
+  //undraw the shape
+  function unDraw() {
+    current.forEach((index) =>
+      squares[currentPosition + index].classList.remove("block")
+    );
+  }
+
+  //move down shape
+
+  function moveDown() {
+    unDraw();
+    currentPosition = currentPosition += width;
+    draw();
+    freeze();
+  }
+
+  //move left and prevent collisions with shapes moving left
+  function moveRight() {
+    unDraw();
+    const isAtRightEdge = current.some(
+      (index) => (currentPosition + index) % width === width - 1
+    );
+    if (!isAtRightEdge) currentPosition += 1;
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("block2")
+      )
+    ) {
+      currentPosition -= 1;
+    }
+    draw();
+  }
+
+  function moveLeft() {
+    unDraw();
+    const isAtLeftEdge = current.some(
+      (index) => (currentPosition + index) % width === 0
+    );
+    if (!isAtLeftEdge) currentPosition -= 1;
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("block2")
+      )
+    ) {
+      currentPosition += 1;
+    }
+    draw();
+  }
+
+  function rotate() {
+    unDraw();
+    currentRotation++;
+    if (currentRotation == current.length) {
+      currentRotation = 0;
+    }
+    current = theTetrominoes[random][currentRotation];
+    draw();
+  }
+  draw();
+});
